bb56ee5b1117936ee04e8814100a6adb97d5705a
Yukit02 <yukijobit02@gmail.com>
2021-08-15 21:52:58 +0900

    Refactor to API_KEY

--- src/request.js
+++ src/request.js
@@ -1,15 +1,9 @@
-const API_KEY = 'ff8ecf3cbec1f3079547a6eeab960fa3';
-
-export const requests ={
-    feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
-    feachNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
-    feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10759`,
-    feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
-    feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-    feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
-}
-
-export const fetchMovieImage = (movieId) => {
-  return `/movie/${movieId}?api_key=${API_KEY}`
+export const requests = {
+    feachTrending:`/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-us`,
+    feachNetflixOriginals:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_networks=213`,
+    feactTopRated:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&languager=en-us`,
+    feactActionMovies:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=10759`,
+    feactComedyMovies:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=35`,
+    feactRomanceMovies:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=10749`,
+    feactDocumentMovies:`/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=99`,
 }
