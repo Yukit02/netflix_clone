0c3a7c6edaaec95b1034ae7e01dff493f3889612
Yukit02 <yukijobit02@gmail.com>
2021-08-15 19:19:16 +0900

    Removed Horrors

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
       <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
       <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
       <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
--- src/request.js
+++ src/request.js
@@ -6,7 +6,6 @@ export const requests ={
     feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
     feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
     feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
-    feactHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_genres=27`,
     feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
     feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
 }
