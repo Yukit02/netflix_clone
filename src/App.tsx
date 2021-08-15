01fef526cc34581c8c0c3df7373626c66062357d
Yukit02 <yukijobit02@gmail.com>
2021-08-15 20:25:00 +0900

    Create banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import { Row } from './Row';
+import { Banner } from './Banner';
 import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGUINALS"
         fetchUrl={requests.feachNetflixOriginals}
